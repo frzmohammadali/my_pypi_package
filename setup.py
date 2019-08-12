96d12da422cd1d4a644f4742aeed6aa15960e3f5
mforouzesh <mforouzesh@mpi-sws.org>
2019-08-12 19:07:16 +0200

    update setup.py the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="my_pypi_package",
-    version="0.0.1",
+    version="0.0.2",
     author="Mohammadali",
     author_email="mforouzesh@mpi-sws.org",
     description="my First Package",
